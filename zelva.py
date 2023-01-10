d7c52c892ad7b1768db8c08cf6a01c18fca83d94
alzbetaklonfarova <83401954+alzbetaklonfarova@users.noreply.github.com>
2023-01-10 11:19:53 +0100

    Add files via upload

---
+++ zelva.py
@@ -0,0 +1,37 @@
+from turtle import exitonclick, forward, right, left, shape, goto, pendown, penup
+from math import sqrt
+from random import randint
+
+shape ('turtle')
+penup()
+goto(-400,0)
+pendown()
+
+def nakresli(a):
+
+    
+    left(90)
+    forward(a)
+    right(90)
+    forward(a)
+    right(135)
+    forward(sqrt(a*a * 2))
+    left(135)
+    forward(a)
+    left(90)
+    forward(a)
+
+    left(45)
+    forward((a/100)*72)
+    left(90)
+    forward((a/100)*72)
+    left(90)
+    forward(sqrt(a*a + a*a))
+    left(45)
+
+nakresli(randint(50, 180))
+nakresli(randint(50, 180))
+nakresli(randint(50, 180))
+nakresli(randint(50, 180))
+nakresli(randint(50, 180))
+exitonclick()
